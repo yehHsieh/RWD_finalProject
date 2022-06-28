1d60e74759cdd60d1121e46aabc45c5d4386b661
yehHsieh <york0704@gmail.com>
2022-06-28 21:36:51 +0800

    RWD final project

---
+++ RWD_finalProject/js/layout.js
@@ -0,0 +1,18 @@
+// $(document).ready(function() {
+//     $('.showMenu').on('click',  function(e){
+//        e.preventDefault();
+//        $('body').toggleClass('showList');
+//    });
+
+//    $('.showMenu').click(function(event) {
+//     $('body').removeClass('showList');
+//   });
+//  });
+
+
+ $(document).ready(function() {
+  $('.showMenu').on('click',  function(e){
+     e.preventDefault();
+     $('body').toggleClass('showList');
+ });
+});
